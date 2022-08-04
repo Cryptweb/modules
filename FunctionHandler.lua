01ff82d67c269b4a96ed291db5722cb172d796ea
evan <58373834+Cryptweb@users.noreply.github.com>
2022-08-04 21:50:26 +0300

    Update FunctionHandler.lua

--- FunctionHandler.lua
+++ FunctionHandler.lua
@@ -14,3 +14,33 @@ function f.getFunctions()
 end
     
 return f
+
+--[[
+
+usage:
+
+-- // Defining the module
+local f = loadstring(game:HttpGet('https://raw.githubusercontent.com/Cryptweb/modules/main/FunctionHandler.lua'))()
+
+-- // Creating a function to redeem all of the game codes (Size Legends)
+f.add('redeemCodes', function()
+    local player = game:GetService('Players').LocalPlayer
+    local remote = player.PlayerGui.Codes.MainFrame.Redeem
+    
+    for _, v in pairs(player.Codes:GetChildren()) do
+        if (v:IsA('BoolValue')) then
+            remote.Redeemed:FireServer(v.Name)
+        end
+    end
+end)
+
+-- // Defining the variable that gets all the functions
+local functions = functionHandler.getFunctions()
+
+-- // If it fails to get/find the functions
+if (not functions) then return print('failed to find functions') end
+
+-- // Using the redeem game codes function
+functions.redeemCodes()
+
+]]
